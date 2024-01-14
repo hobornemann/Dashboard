be6321540369a9b8fb203419cd9f7c06aacb0440
Hans-Olov <hobornemann@gmail.com>
2024-01-14 23:47:21 +0100

    broke down full page dynamic html and updated the dynamic pieces only

---
+++ README.md
@@ -0,0 +1,23 @@
+
+
+ERROR-HANDLING
+
+API-Calls / Network errors:
+> default variants
+> messages
+
+
+
+User-input:>
+> validation of usr input
+> security aspects
+
+
+User-interaction:
+> double-clicks ?
+
+
+
+Future Maintenance & Development:
+> Need to make sure that id-numbers are universal for the document
+
--- main.js
+++ main.js
@@ -28,8 +28,8 @@ let dashboard;
 // -------------------------------------------------------------
 
 const bodyContainer = document.querySelector(".body-container");
-const dateTime = document.querySelector(".dateTime");
-const mainHeading = document.querySelector(".main-heading");
+
+const mainHeadingElement = document.querySelector(".main-heading");
 const webLinksContainer = document.querySelector(".webLinks-container");
 const webLinkFavicon = document.querySelector(".webLink-favicon");
 const webLinkHeading = document.querySelector(".webLink-heading");
@@ -50,20 +50,38 @@ const buttonChangeBackground = document.querySelector(".button-change-background
 
 window.addEventListener("DOMContentLoaded", () => {
   
+  // Reset dashboard in localStorage
+  localStorage.setItem("dashboard", null);
+  
   let dashboardInLocalStorage = JSON.parse(localStorage.getItem("dashboard"));
+  
+  console.log("dashboardInLocalStorage: ")
+  console.log(dashboardInLocalStorage)
+  
+  
+
   if(dashboardInLocalStorage == null) {
+    console.log("dashboardInLocalStorage == null");
+
     getDefaultDashboardAndStoreInLocalStorage();
   } else{
+
     dashboard = dashboardInLocalStorage;
   }
-  dashboard.dateTime = getDateTime();
-  //loadPage(dashboard) // TODO:  uncomment this line
+  
+  // TODO: update dashboard with current weather forecasts 
+  // TODO: update favicon / FaviconUrl
+  console.log(dashboard);
+
+  updateBodyContainerStepByStep();
+  //displayBodyContainer(dashboard) // TODO:  throws error
   //displayCurrentTime();  TODO: activate clock by uncommenting this line
 });
 
-mainHeading.addEventListener("input", () => {
-  dashboard.mainHeading = mainHeading.textContent;
+mainHeadingElement.addEventListener("input", () => {
+  dashboard.mainHeading = mainHeadingElement.textContent;
   localStorage.setItem("dashboard", JSON.stringify(dashboard));
+  console.log(JSON.parse(localStorage.getItem("dashboard")));
 })
 
 buttonOpenAddWebLinkForm.addEventListener("click", () =>{
@@ -93,117 +111,102 @@ buttonChangeBackground.addEventListener("click", changeBackGroundImage())
 // FUNCTIONS
 // -------------------------------------------------------------
 
-//TODO: Insert html-markup for news cards and store in localStorage 
-function loadPage(dashboard){
+function updateBodyContainerStepByStep(){
 
-  let bodyContainerHtml = 
-  `<header class="header-container">
-    <p class="dateTime">${dashboard.dateTime}</p>
-  </header>
-  <div class="empty-container1"></div>
-  <main class="main-container">
-      <h1 class="main-heading" contenteditable="true">${dashboard.mainHeading}</h1>
-      <section class="cards-container"> 
-        <article class="card">
-          <h3 class="card-heading">Snabblänkar</h3>
-          <div class="webLinks-container">`;
+  displayDateTime(); 
+  displayMainHeading();
+  displayWebLinkCards();
+  displayWeatherCards();
+  displayNotes();
+}
 
-dashboard.webLinks.map(webLink => {
-  bodyContainerHtml = bodyContainerHtml + 
-  `<button class="webLink-card small-card" id="${webLink.id}">
-    <img class="webLink-favicon" src="${webLink.webLinkFaviconUrl}">
-    <div class="webLink-heading">${webLink.webLinkHeading}</div>
-    <div class="delete-webLink">x</div>
-  </button>`;
-});
 
-bodyContainerHtml = bodyContainerHtml + 
-  `</div>
-  <button class="button-open-add-webLink-form button">Ny webblänk</button>
-  <form class="form-add-webLink"> 
-    <input class="input-webLink-url input" type="text" name="input-webLink-url" id="input-webLink-url" placeholder="Kopiera in webblänken här, tex www.google.se">          
-    <input class="input-webLink-heading input" type="text" name="input-webLink-heading" id="input-webLink-heading" placeholder="Skriv namnet på länken här, tex Google"> 
-    <input class="button-add-new-webLink button" type="submit" value="OK">
-  </form>
-</article>
-<article class="card">
-<h3 class="card-heading">Väder</h3>
-<div class="weather-container">`;
 
-dashboard.weatherForecasts.map(weatherForecast => {
-  bodyContainerHtml = bodyContainerHtml + 
-  `<div class="weather-card small-card">
-  <img class="weather-icon" src="${dashboard.weatherForecast.weatherIconUrl}"/>
-  <div class="weather-day-temperature-comment">
-    <div class="weather-day-heading">${dashboard.weatherForecast.dayHeading}</div>
-    <div class="weather-temperature-comment">
-      <div class="weather-temperature">&nbsp;${dashboard.weatherForecast.temperature}&deg;C&nbsp;</div>
-      <div class="weather-comment">&nbsp;${dashboard.weatherForecast.weatherComment}&nbsp;</div>
-    </div>
-  </div>
-</div>  
-`;
-});
 
-bodyContainerHtml = bodyContainerHtml + 
-`</article>
-<article class="card">
-  <h3 class="card-heading">Nyheter</h3>
-  <div class="news-container">`;
-    
-//TODO: Insert html-markup for news cards and store in localStorage 
-  
-  bodyContainerHtml = bodyContainerHtml + 
-  `</div>  
-</article>
-<article class="card">
-  <h3 class="card-heading">Anteckningar</h3>
-  <div class="notes-card small-card">
-    <p class="notes" contenteditable="true">${dashboard.notes}</p>  
-  </div>
-</article>
-</section> 
-</main>
-<div class="empty-container2"></div>
-<footer class="footer-container">
-<button class="button-change-background button">Byt bakgrundsbild</button>
-</footer>
-`;
 
-  bodyContainer.innerHTML = bodyContainerHtml;
+
+function displayDateTime(){
+  const dateTimeElement = document.querySelector(".dateTime");
+  dateTimeElement.textContent = dashboard.dateTime;
 };
+
+function displayMainHeading(){
+  mainHeadingElement.textContent = dashboard.mainHeading;
+}
+
+function displayWebLinkCards(){
+  let webLinksContainerHtml = "";
+  dashboard.webLinks.map(webLink => {
+    webLinksContainerHtml = webLinksContainerHtml + 
+    `<a href="${webLink.webLinkUrl}" target="_blank">
+      <button class="webLink-card small-card" id="${webLink.id}">
+        <img class="webLink-favicon" src="${webLink.webLinkFaviconUrl}">
+        <div class="webLink-heading">${webLink.webLinkHeading}</div>
+        <div class="delete-webLink">x</div>
+      </button>
+    </a>`;
+  });
+  webLinksContainer.innerHTML = webLinksContainerHtml;
+};
+
+
+function displayWeatherCards(){
+  let weatherContainerHtml = "";
+  dashboard.weatherForecasts.map(weatherForecast => {
+    weatherContainerHtml = weatherContainerHtml + 
+    `<div class="weather-card small-card">
+      <img class="weather-icon" src="${weatherForecast.weatherIconUrl}">
+      <div class="weather-day-temperature-comment">
+        <div class="weather-day-heading">${weatherForecast.dayHeading}</div>
+        <div class="weather-temperature-comment">
+          <div class="weather-temperature">&nbsp${weatherForecast.temperature}&deg&nbsp</div>
+          <div class="weather-comment">&nbsp${weatherForecast.weatherComment}&nbsp</div>
+        </div>
+      </div>
+    </div>  
+  `;
+  });
+  weatherContainer.innerHTML = weatherContainerHtml;
+};
+
+function displayNotes(){
+  notes.innerHTML = dashboard.notes;
+};
+
+
 
 // TODO: expand the dashboard object with news-data
 function getDefaultDashboardAndStoreInLocalStorage(){
-  let dashboard = {
+  
+  dashboard = {
     dateTime: "14:55  2023-12-13",
     mainHeading: "My Dashboard",
     webLinks: [
       { 
         id: 1,
-        webLinkFaviconUrl: "",
+        webLinkFaviconUrl: "/images/default-icon.png",
         webLinkHeading: "Google",
         webLinkUrl: "www.google.se",
       },
     ],
     weatherForecasts: [
       {
-        weatherIconUrl: "",
+        weatherIconUrl: "/images/default-icon.png",
         dayHeading: "Idag",
-        temperature: "-4&degr;C",
+        temperature: "-4&deg",
         weatherComment: "Sol och moln",       
       },
       {
-        weatherIcon: "",
+        weatherIconUrl: "/images/default-icon.png",
         dayHeading: "I morgon",
-        temperature: "-1&degr;C",
-        weatherDetails: "Sol och klart"        
+        temperature: "-1&deg",
+        weatherComment: "Sol och klart"        
       },
       {
-        weatherIcon: "",
+        weatherIconUrl: "/images/default-icon.png",
         dayHeading: "I övermorgon",
-        temperature: "+2&degr;C",
-        weatherDetails: "Snö och moln"        
+        temperature: "+2&deg",
+        weatherComment: "Snö och moln"        
       }
     ],
     news: [
@@ -212,6 +215,7 @@ function getDefaultDashboardAndStoreInLocalStorage(){
     notes: "Här kan du skriva dina anteckningar. Anteckningarna sparas automatiskt.",
   };
 
+  dashboard.dateTime = getDateTime();
   localStorage.setItem("dashboard", JSON.stringify(dashboard));
  
 }
@@ -223,7 +227,9 @@ function getDateTime(){
                   + ((currentDateTime.getMinutes() < 10)?"0":"")  
                   + currentDateTime.getMinutes() + "  " 
                   + currentDateTime.getFullYear() + "-"
+                  + ((currentDateTime.getMonth() + 1 < 10)?"0":"") 
                   + (currentDateTime.getMonth()+1)  + "-" 
+                  + ((currentDateTime.getDate() < 10)?"0":"") 
                   + currentDateTime.getDate(); 
   return dateTime;
 };
@@ -265,9 +271,7 @@ function addNewWebLink(){
  
   webLinksContainerHtml = webLinksContainerHtml + newWebLinkButtonHtml;
   webLinksContainer.innerHTML = webLinksContainerHtml;
-
   // TODO:  Do I need to call a display function to show the added webLinkButton or will that happen automatically?
-  
   formAddWebLink.classList.add("hidden");
 }
 
@@ -283,6 +287,100 @@ function changeBackGroundImage(){
 // OTHER CODE
 // --------------------------------------------------------------
 
+
+//Insert html-markup for news cards and store in localStorage 
+/* function displayBodyContainer(){
+
+  console.log("dateTime");
+  console.log(dashboard.dateTime);
+
+  let bodyContainerHtml = 
+  `<header class="header-container">
+    <p class="dateTime">${dashboard.dateTime}</p>
+  </header>
+  <div class="empty-container1"></div>
+  <main class="main-container">
+      <h1 class="main-heading" contenteditable="true">${dashboard.mainHeading}</h1>
+      <section class="cards-container"> 
+        <article class="card">
+          <h3 class="card-heading">Snabblänkar</h3>
+          <div class="webLinks-container">`;
+
+console.log("B")
+
+dashboard.webLinks.map(webLink => {
+  bodyContainerHtml = bodyContainerHtml + 
+  `<button class="webLink-card small-card" id="${webLink.id}">
+    <img class="webLink-favicon" src="${webLink.webLinkFaviconUrl}">
+    <div class="webLink-heading">${webLink.webLinkHeading}</div>
+    <div class="delete-webLink">x</div>
+  </button>`;
+});
+
+console.log("C")
+
+bodyContainerHtml = bodyContainerHtml + 
+  `</div>
+  <button class="button-open-add-webLink-form button">Ny webblänk</button>
+  <form class="form-add-webLink"> 
+    <input class="input-webLink-url input" type="text" name="input-webLink-url" id="input-webLink-url" placeholder="Kopiera in webblänken här, tex www.google.se">          
+    <input class="input-webLink-heading input" type="text" name="input-webLink-heading" id="input-webLink-heading" placeholder="Skriv namnet på länken här, tex Google"> 
+    <input class="button-add-new-webLink button" type="submit" value="OK">
+  </form>
+</article>
+<article class="card">
+<h3 class="card-heading">Väder</h3>
+<div class="weather-container">`;
+
+console.log("D")
+
+dashboard.weatherForecasts.map(weatherForecast => {
+  bodyContainerHtml = bodyContainerHtml + 
+  `<div class="weather-card small-card">
+  <img class="weather-icon" src="${weatherForecast.weatherIconUrl}">
+  <div class="weather-day-temperature-comment">
+    <div class="weather-day-heading">${weatherForecast.dayHeading}</div>
+    <div class="weather-temperature-comment">
+      <div class="weather-temperature">&nbsp${weatherForecast.temperature}&deg&nbsp</div>
+      <div class="weather-comment">&nbsp${weatherForecast.weatherComment}&nbsp</div>
+    </div>
+  </div>
+</div>  
+`;
+});
+
+console.log("E")
+
+bodyContainerHtml = bodyContainerHtml + 
+`</article>
+<article class="card">
+  <h3 class="card-heading">Nyheter</h3>
+  <div class="news-container">`;
+    
+  console.log("F")
+//Insert html-markup for news cards and store in localStorage 
+  
+  bodyContainerHtml = bodyContainerHtml + 
+  `</div>  
+</article>
+<article class="card">
+  <h3 class="card-heading">Anteckningar</h3>
+  <div class="notes-card small-card">
+    <p class="notes" contenteditable="true">${dashboard.notes}</p>  
+  </div>
+</article>
+</section> 
+</main>
+<div class="empty-container2"></div>
+<footer class="footer-container">
+<button class="button-change-background button">Byt bakgrundsbild</button>
+</footer>
+`;
+
+console.log("G")
+  bodyContainer.innerHTML = bodyContainerHtml;
+  console.log("H")
+}; */
 
 
 /* import './style.css'
